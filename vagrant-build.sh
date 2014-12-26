0b560ae31f687692f4c435710e03ed91a33f6cf7
KensoDev <avi@kensodev.com>
2014-12-26 14:47:33 +0200

    Add github.com to list of known hosts when provisioning the machine

---
+++ vagrant-build.sh
@@ -0,0 +1,2 @@
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile /dev/null\n" >> ~/.ssh/config
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile /dev/null\n" >> /home/vagrant/.ssh/config
